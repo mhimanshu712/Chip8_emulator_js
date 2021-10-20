68a9eba321241e560c2144c57f0cb47bf488025a
Himanshu Mishra <mhimanshu712@gmail.com>
2021-10-20 14:24:48 +0530

    Keyboard done

--- Keyboard.js
+++ Keyboard.js
@@ -1,5 +1,23 @@
 class Keyboard {
     constructor() {
+        this.KEYMAP = {
+            49: 0x1, // 1
+            50: 0x2, // 2
+            51: 0x3, // 3
+            52: 0xc, // 4
+            81: 0x4, // Q
+            87: 0x5, // W
+            69: 0x6, // E
+            82: 0xD, // R
+            65: 0x7, // A
+            83: 0x8, // S
+            68: 0x9, // D
+            70: 0xE, // F
+            90: 0xA, // Z
+            88: 0x0, // X
+            67: 0xB, // C
+            86: 0xF  // V
+        }
 
         this.keysPressed = [];
         this.onNextKeyPress = null;
@@ -22,4 +40,11 @@ class Keyboard {
             this.onNextKeyPress = null;
         }
     }
+
+    onKeyUp(event) {
+        let key = this.KEYMAP[event.which];
+        this.keysPressed[key] = false;
+    }
 }
+
+export default Keyboard;
